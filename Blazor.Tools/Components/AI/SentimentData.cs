22631ab36a3d362b985fdaedb472a36e194f9ea1
solomiofactura <solomiofactura@gmail.com>
2024-06-28 16:58:49 +0800

    Added sample AIChatBot component

---
+++ Blazor.Tools/Components/AI/SentimentData.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ML.Data;
+
+namespace Blazor.Tools.Components.AI
+{
+    public class SentimentData
+    {
+        [LoadColumn(0)]
+        public bool Sentiment { get; set; }
+
+        [LoadColumn(1)]
+        public string SentimentText { get; set; }
+    }
+
+}
