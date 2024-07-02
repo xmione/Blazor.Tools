3b70e01a97ec42e08da15d4df8730edc2fb31f53
solomiofactura <solomiofactura@gmail.com>
2024-07-02 10:58:42 +0800

    Added a menu in the console app.

--- Blazor.Tools.ConsoleApp/Program.cs
+++ Blazor.Tools.ConsoleApp/Program.cs
@@ -10,84 +10,84 @@ namespace Blazor.Tools.ConsoleApp
     {
         public static void Main(string[] args)
         {
-
-            args = ["5"];
-
             var answerConfig = new AnswerConfig();
-            var entries = new List<NqEntry>();
             var mlFolder = @"C:\repo\Blazor.Tools\Blazor.Tools\Data\ML";
             var fileName = string.Empty;
             var trainingFilePath = string.Empty;
             var outputFilePath = string.Empty;
             var dateToday = DateTime.Now.ToString("yyyy-dd-MM_HH-mm-ss");
 
-            switch (args[0])
+            while (true)
             {
-                case "1":
-                    ConvertTabDelimitedFileToCsv(mlFolder);
-                    break;
-                case "2":
-                    answerConfig = new AnswerConfig
-                    {
-                        StartProperty = "start_byte",
-                        EndProperty = "end_byte",
-                        IsBytePosition = true
-                    };
+                Console.WriteLine("Please choose an option:");
+                Console.WriteLine("[1] - Convert Tab Delimited Yelp file to CSV");
+                Console.WriteLine("[2] - Decompress and parse v1.0-simplified_nq-dev-all.jsonl.gz");
+                Console.WriteLine("[3] - Decompress and parse v1.0-simplified_simplified-nq-train.jsonl.gz");
+                Console.WriteLine("[4] - Parse and save v1.0-simplified_nq-dev-all.jsonl");
+                Console.WriteLine("[5] - Parse and save v1.0-simplified_simplified-nq-train.jsonl");
+                Console.WriteLine("[6] - Exit");
 
-                    fileName = "v1.0-simplified_nq-dev-all.jsonl";
-                    DecompressAndParseJsonlFile(mlFolder, fileName, answerConfig);
-                    break;
-                case "3":
-                    answerConfig = new AnswerConfig
-                    {
-                        StartProperty = "start_token",
-                        EndProperty = "end_token",
-                        IsBytePosition = false
-                    };
+                var choice = Console.ReadLine();
 
+                switch (choice)
+                {
+                    case "1":
+                        ConvertTabDelimitedFileToCsv(mlFolder);
+                        break;
+                    case "2":
+                        answerConfig = new AnswerConfig
+                        {
+                            StartProperty = "start_byte",
+                            EndProperty = "end_byte",
+                            IsBytePosition = true
+                        };
 
-                    fileName = "v1.0-simplified-nq-train.jsonl";
-                    DecompressAndParseJsonlFile(mlFolder, fileName, answerConfig);
-                    break;
-                case "4":
-                    answerConfig = new AnswerConfig
-                    {
-                        StartProperty = "start_byte",
-                        EndProperty = "end_byte",
-                        IsBytePosition = true
-                    };
+                        fileName = "v1.0-simplified_nq-dev-all.jsonl";
+                        DecompressAndParseJsonlFile(mlFolder, fileName, answerConfig);
+                        break;
+                    case "3":
+                        answerConfig = new AnswerConfig
+                        {
+                            StartProperty = "start_token",
+                            EndProperty = "end_token",
+                            IsBytePosition = false
+                        };
 
-                    fileName = "v1.0-simplified_nq-dev-all.jsonl";
-                    outputFilePath = Path.Combine(mlFolder, fileName);
-                    trainingFilePath = Path.Combine(mlFolder, fileName + $"-{dateToday}.txt");
-                    NqEntryExtensions.ParseJsonlFile(outputFilePath, answerConfig, trainingFilePath);
-                    break;
-                case "5":
-                    answerConfig = new AnswerConfig
-                    {
-                        StartProperty = "start_token",
-                        EndProperty = "end_token",
-                        IsBytePosition = false
-                    };
+                        fileName = "v1.0-simplified-nq-train.jsonl";
+                        DecompressAndParseJsonlFile(mlFolder, fileName, answerConfig);
+                        break;
+                    case "4":
+                        answerConfig = new AnswerConfig
+                        {
+                            StartProperty = "start_byte",
+                            EndProperty = "end_byte",
+                            IsBytePosition = true
+                        };
 
-                    fileName = "v1.0-simplified-nq-train.jsonl";
-                    outputFilePath = Path.Combine(mlFolder, fileName);
-                    trainingFilePath = Path.Combine(mlFolder, fileName + $"-{dateToday}.txt");
-                    NqEntryExtensions.ParseJsonlFile(outputFilePath, answerConfig, trainingFilePath);
-                    break;
-                case "6":
-                    answerConfig = new AnswerConfig
-                    {
-                        StartProperty = "start_token",
-                        EndProperty = "end_token",
-                        IsBytePosition = false
-                    };
+                        fileName = "v1.0-simplified_nq-dev-all.jsonl";
+                        outputFilePath = Path.Combine(mlFolder, fileName);
+                        trainingFilePath = Path.Combine(mlFolder, fileName + $"-{dateToday}.txt");
+                        NqEntryExtensions.ParseJsonlFile(outputFilePath, answerConfig, trainingFilePath);
+                        break;
+                    case "5":
+                        answerConfig = new AnswerConfig
+                        {
+                            StartProperty = "start_token",
+                            EndProperty = "end_token",
+                            IsBytePosition = false
+                        };
 
-                    fileName = "v1.0-simplified-nq-train.jsonl";
-                    outputFilePath = Path.Combine(mlFolder, fileName);
-                    trainingFilePath = Path.Combine(mlFolder, fileName + $"-{dateToday}.txt");
-                    NqEntryExtensions.ParseJsonlFile(outputFilePath, answerConfig, trainingFilePath);
-                    break;
+                        fileName = "v1.0-simplified-nq-train.jsonl";
+                        outputFilePath = Path.Combine(mlFolder, fileName);
+                        trainingFilePath = Path.Combine(mlFolder, fileName + $"-{dateToday}.txt");
+                        NqEntryExtensions.ParseJsonlFile(outputFilePath, answerConfig, trainingFilePath);
+                        break;
+                    case "6":
+                        return; // Exit the program
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
             }
         }
 
