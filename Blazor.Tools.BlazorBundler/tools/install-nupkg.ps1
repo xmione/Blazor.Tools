7c1d352b136cae8162248594181692df0ff47b80
solomiofactura <solomiofactura@gmail.com>
2024-07-18 22:14:35 +0800

    Added nuget packages and index.json file.

--- Blazor.Tools.BlazorBundler/tools/install-nupkg.ps1
+++ Blazor.Tools.BlazorBundler/tools/install-nupkg.ps1
@@ -3,19 +3,35 @@
     [string]$DestinationPath = "$env:ProgramFiles\NuGet\Packages"
 )
 
-# Ensure PackageManagement and NuGet provider are available
-if (-not (Get-Module -ListAvailable -Name PackageManagement)) {
-    Install-Module -Name PackageManagement -Force -SkipPublisherCheck
-}
+try {
+    # Ensure PackageManagement and NuGet provider are available
+    if (-not (Get-Module -ListAvailable -Name PackageManagement)) {
+        Write-Host "Installing PackageManagement module..."
+        Install-Module -Name PackageManagement -Force -SkipPublisherCheck
+    }
 
-if (-not (Get-PackageSource -Name 'Nuget.org')) {
-    Register-PackageSource -Name Nuget.org -ProviderName NuGet -Location "https://api.nuget.org/v3/index.json"
-}
+    # This is not needed for local nuget packages
+    <#
+        if (-not (Get-PackageSource -Name 'Nuget.org')) {
+            Write-Host "Registering Nuget.org package source..."
+            Register-PackageSource -Name Nuget.org -ProviderName NuGet -Location "https://api.nuget.org/v3/index.json"
+        }
+    #>
 
-# Ensure the destination directory exists
-if (-not (Test-Path -Path $DestinationPath)) {
-    New-Item -ItemType Directory -Path $DestinationPath -Force
-}
+    # Ensure the destination directory exists
+    if (-not (Test-Path -Path $DestinationPath)) {
+        Write-Host "Creating destination directory: $DestinationPath"
+        New-Item -ItemType Directory -Path $DestinationPath -Force
+    }
 
-# Install the package
-Install-Package -Source $PackagePath -DestinationPath $DestinationPath -ProviderName NuGet -Force
+    cls
+    Write-Host "Starting installation of package from $PackagePath to $DestinationPath"
+
+    # Install the package
+    Install-Package -Source $PackagePath -DestinationPath $DestinationPath -ProviderName NuGet -Force
+    
+    Write-Host "Completed the installation of package: $PackagePath"
+} catch {
+    Write-Error "An error occurred during the package installation: $_"
+    exit 1
+}
