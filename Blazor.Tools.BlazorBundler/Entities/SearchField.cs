4634ffdd4119495f4ecd6bac001a6241ea70dbf2
solomiofactura <solomiofactura@gmail.com>
2024-07-31 17:08:49 +0800

    Updated Issue documentation. Improved SearchField

--- Blazor.Tools.BlazorBundler/Entities/SearchField.cs
+++ Blazor.Tools.BlazorBundler/Entities/SearchField.cs
@@ -4,5 +4,6 @@
     {
         public string TableName { get; set; } = default!;
         public string FieldName { get; set; } = default!;
+        public LookupFieldConditionEnum MatchCondition { get; set; } = default!;
     }
 }
