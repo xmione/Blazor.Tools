d51ffbaaff46f7eb2d66625138d68560a5f9e2e9
solomiofactura <solomiofactura@gmail.com>
2024-08-13 17:30:42 +0800

    #8 - Added TestTableGridInternals component.

---
+++ Blazor.Tools.BlazorBundler/Entities/TableColumnDefinitions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Tools.BlazorBundler.Entities
+{
+    public class TableColumnDefinition
+    {
+        public string ColumnName { get; set; } = string.Empty;
+        public string HeaderText { get; set; } = string.Empty;
+        public Type ColumnType { get; set; } = default!;
+        public RenderFragment<object> CellTemplate { get; set; } = default!;
+        public EventCallback<object> ValueChanged { get; set; } = default!;
+    }
+
+}
