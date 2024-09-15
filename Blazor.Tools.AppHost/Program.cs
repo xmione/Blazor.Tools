b5e22123603127f2c871829fd9dc7751831a2948
solomiofactura <solomiofactura@gmail.com>
2024-09-15 20:09:47 +0800

    Added .Net Aspire Test Project for integration testing.

--- Blazor.Tools.AppHost/Program.cs
+++ Blazor.Tools.AppHost/Program.cs
@@ -1,5 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Blazor_Tools>("blazor-tools");
 
+// Register the 'webfrontend' resource
+builder.Services.AddHttpClient("webfrontend", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7031/");  // Or your actual frontend URL
+});
 builder.Build().Run();
