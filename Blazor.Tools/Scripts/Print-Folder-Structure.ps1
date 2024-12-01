2300a1535f27a752e0723e3b11b69f38317745ec
Solomio Sisante <solomiosisante@gmail.com>
2024-11-30 21:53:11 -0800

    corrected name

--- Blazor.Tools/Scripts/Print-Folder-Structure.ps1
+++ Blazor.Tools/Scripts/Print-Folder-Structure.ps1
@@ -5,5 +5,5 @@ Write-Output "Removing modules..."
 Write-Output "Removing module Print-Folder-Structure"
 Remove-Module Print-Folder-Structure
 Write-Output "Importing modules..."
-Write-Output "Importing module Print-DirectoryStructure"
+Write-Output "Importing module Print-Folder-Structure"
 Import-Module $ScriptDir\Print-Folder-Structure.psm1
